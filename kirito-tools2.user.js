ec37b4618f07a549cf5e4984569a57532f2f7f4b
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-05-17 13:03:40 +0800

    v0.3.3 查看其他玩家的轉生點配置，格式比照原生，兩種查看模式皆可看到

--- kirito-tools2.user.js
+++ kirito-tools2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kirito Tools
 // @namespace    -
-// @version      0.3.2
+// @version      0.3.3
 // @description  mykirito.com 的界面調整，不包含任何自動操作。
 // @author       LianSheng
 // @include      https://mykirito.com/*
@@ -9,6 +9,32 @@
 // @grant        GM_getValue
 // @license      MIT
 // ==/UserScript==
+
+// （詳細資料）顯示其他玩家的轉生點加成
+function addPointsToDetail(tableRows, playerData) {
+    tableRows[4].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.hp > 0? "(+"+playerData.rattrs.hp*10+")" : ""}</span>`;
+    tableRows[5].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.atk > 0? "(+"+playerData.rattrs.atk+")" : ""}</span>`;
+    tableRows[6].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.def > 0? "(+"+playerData.rattrs.def+")" : ""}</span>`;
+    tableRows[7].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.stm > 0? "(+"+playerData.rattrs.stm+")" : ""}</span>`;
+    tableRows[8].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.agi > 0? "(+"+playerData.rattrs.agi+")" : ""}</span>`;
+    tableRows[9].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.spd > 0? "(+"+playerData.rattrs.spd+")" : ""}</span>`;
+    tableRows[10].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.tec > 0? "(+"+playerData.rattrs.tec+")" : ""}</span>`;
+    tableRows[11].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.int > 0? "(+"+playerData.rattrs.int+")" : ""}</span>`;
+    tableRows[12].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.lck > 0? "(+"+playerData.rattrs.lck+")" : ""}</span>`;
+}
+
+// （能力比對）顯示其他玩家的轉生點加成
+function addPointsToCompare(tableRows, playerData) {
+    tableRows[6].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.hp > 0? "(+"+playerData.rattrs.hp*10+")" : ""}</span>`;
+    tableRows[7].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.atk > 0? "(+"+playerData.rattrs.atk+")" : ""}</span>`;
+    tableRows[8].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.def > 0? "(+"+playerData.rattrs.def+")" : ""}</span>`;
+    tableRows[9].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.stm > 0? "(+"+playerData.rattrs.stm+")" : ""}</span>`;
+    tableRows[10].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.agi > 0? "(+"+playerData.rattrs.agi+")" : ""}</span>`;
+    tableRows[11].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.spd > 0? "(+"+playerData.rattrs.spd+")" : ""}</span>`;
+    tableRows[12].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.tec > 0? "(+"+playerData.rattrs.tec+")" : ""}</span>`;
+    tableRows[13].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.int > 0? "(+"+playerData.rattrs.int+")" : ""}</span>`;
+    tableRows[14].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.lck > 0? "(+"+playerData.rattrs.lck+")" : ""}</span>`;
+}
 
 (async function () {
     'use strict';
@@ -57,7 +83,8 @@
 
                 let token = localStorage.getItem("token");
                 let playerId = url.match(/\/([0-9a-zA-Z]+?)$/)[1];
-                let tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > table tr");
+                let buttonTypes = document.querySelectorAll("div#root > div > div > div:nth-child(1) > button");
+
 
                 let playerData = await fetch(`https://mykirito.com/api/profile/${playerId}`, {
                     "headers": {
@@ -65,19 +92,47 @@
                     }
                 }).then(r => r.json()).then(t => t.profile);
 
-                // 顯示其他玩家的轉生點加成
-                tableRows[4].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.hp * 10})</span>`;
-                tableRows[5].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.atk})</span>`;
-                tableRows[6].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.def})</span>`;
-                tableRows[7].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.stm})</span>`;
-                tableRows[8].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.agi})</span>`;
-                tableRows[9].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.spd})</span>`;
-                tableRows[10].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.tec})</span>`;
-                tableRows[11].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.int})</span>`;
-                tableRows[12].querySelector("td").innerHTML += `<span color="rgb(0, 181, 181);"> (+${playerData.rattrs.lck})</span>`;
+                // 哪個按鈕禁止點擊就表示在哪個查看類型
+                let tableRows;
+                if (buttonTypes[0].disabled) {
+                    // （初始化、詳細資料）顯示其他玩家的轉生點加成
+                    tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > table tr");
+                    addPointsToDetail(tableRows, playerData);
+                } else {
+                    // （初始化、能力比對）顯示其他玩家的轉生點加成
+                    tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > div > table:nth-child(2) tr");
+                    addPointsToCompare(tableRows, playerData);
+                }
 
+                // 事件監聽：切換查看類型的兩個按鈕
+                buttonTypes[0].addEventListener("click", function () {
+                    let id = setInterval(() => {
+                        try {
+                            let tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > table tr");
+                            addPointsToDetail(tableRows, playerData);
+
+                            if (document.querySelectorAll("div#root > div > div > div:nth-child(1) > table tr span").length > 0) {
+                                clearInterval(id);
+                            }
+                        } catch (e) {}
+                    }, 100);
+                });
+                buttonTypes[1].addEventListener("click", function () {
+                    let id = setInterval(() => {
+                        try {
+                            let tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > div > table:nth-child(2) tr");
+                            addPointsToCompare(tableRows, playerData);
+
+                            if (document.querySelectorAll("div#root > div > div > div:nth-child(1) > div > table:nth-child(2) tr span").length > 0) {
+                                clearInterval(id);
+                            }
+                        } catch (e) {}
+                    }, 100);
+                });
+
+
+                // 事件監聽：打架的四個按鈕
                 let buttons = document.querySelectorAll("div#root > div > div > div:nth-child(2) > div:nth-last-child(1) button");
-
                 buttons.forEach(button => {
                     button.addEventListener("click", async () => {
                         if (!button.disabled) {
