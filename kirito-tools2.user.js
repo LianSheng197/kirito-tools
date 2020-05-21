e652d1b84d3b73e6e098b32535cfa34782de17a7
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-05-21 12:52:33 +0800

    v0.3.9 顯示戰報雙方配點與玩家連結

--- kirito-tools2.user.js
+++ kirito-tools2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kirito Tools
 // @namespace    -
-// @version      0.3.8
+// @version      0.3.9
 // @description  mykirito.com 的界面調整，不包含任何自動操作。
 // @author       LianSheng
 // @include      https://mykirito.com/*
@@ -36,12 +36,35 @@ function addPointsToCompare(tableRows, playerData) {
     tableRows[14].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${playerData.rattrs.lck > 0? "(+"+playerData.rattrs.lck+")" : ""}</span>`;
 }
 
+// （戰報）顯示雙方的轉生點加成
+function addPointsAndLinkToReport(atkTr, atkData, defTr, defData) {
+    atkTr[6].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.hp > 0? "(+"+atkData.rattrs.hp*10+")" : ""}</span>`;
+    atkTr[7].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.atk > 0? "(+"+atkData.rattrs.atk+")" : ""}</span>`;
+    atkTr[8].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.def > 0? "(+"+atkData.rattrs.def+")" : ""}</span>`;
+    atkTr[9].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.stm > 0? "(+"+atkData.rattrs.stm+")" : ""}</span>`;
+    atkTr[10].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.agi > 0? "(+"+atkData.rattrs.agi+")" : ""}</span>`;
+    atkTr[11].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.spd > 0? "(+"+atkData.rattrs.spd+")" : ""}</span>`;
+    atkTr[12].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.tec > 0? "(+"+atkData.rattrs.tec+")" : ""}</span>`;
+    atkTr[13].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.int > 0? "(+"+atkData.rattrs.int+")" : ""}</span>`;
+    atkTr[14].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${atkData.rattrs.lck > 0? "(+"+atkData.rattrs.lck+")" : ""}</span>`;
+
+    defTr[6].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.hp > 0? "(+"+defData.rattrs.hp*10+")" : ""}</span>`;
+    defTr[7].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.atk > 0? "(+"+defData.rattrs.atk+")" : ""}</span>`;
+    defTr[8].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.def > 0? "(+"+defData.rattrs.def+")" : ""}</span>`;
+    defTr[9].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.stm > 0? "(+"+defData.rattrs.stm+")" : ""}</span>`;
+    defTr[10].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.agi > 0? "(+"+defData.rattrs.agi+")" : ""}</span>`;
+    defTr[11].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.spd > 0? "(+"+defData.rattrs.spd+")" : ""}</span>`;
+    defTr[12].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.tec > 0? "(+"+defData.rattrs.tec+")" : ""}</span>`;
+    defTr[13].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.int > 0? "(+"+defData.rattrs.int+")" : ""}</span>`;
+    defTr[14].querySelector("td").innerHTML += `<span style="color: rgb(0, 181, 181);"> ${defData.rattrs.lck > 0? "(+"+defData.rattrs.lck+")" : ""}</span>`;
+}
+
 // 檢測是否爲行動裝置。目前已知手機版無法正常查看額外資訊也無法正常觸發通知。
 // 由於此腳本涉及請求，避免意料外的狀況，直接避免發送請求應是最穩妥的解法。
 // 正規參照：http://detectmobilebrowsers.com/ (2014)
 function mobileCheck() {
     let check = false;
-    (function (a) {
+    (function(a) {
         if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) {
             check = true;
         }
@@ -50,7 +73,7 @@ function mobileCheck() {
     return check;
 };
 
-(async function () {
+(async function() {
     'use strict';
 
     // 調整介面：使導航欄維持在頁面上方
@@ -101,7 +124,7 @@ function mobileCheck() {
 
         let url = "";
         let lastUrl = "";
-        setInterval(async function () {
+        setInterval(async function() {
             url = location.href;
 
             if (GM_getValue("NotifiTime") != "-1" && Date.now() > GM_getValue("NotifiTime")) {
@@ -137,7 +160,7 @@ function mobileCheck() {
                     }
 
                     // 事件監聽：切換查看類型的兩個按鈕
-                    buttonTypes[0].addEventListener("click", function () {
+                    buttonTypes[0].addEventListener("click", function() {
                         let id = setInterval(() => {
                             try {
                                 let tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > table tr");
@@ -149,7 +172,7 @@ function mobileCheck() {
                             } catch (e) {}
                         }, 100);
                     });
-                    buttonTypes[1].addEventListener("click", function () {
+                    buttonTypes[1].addEventListener("click", function() {
                         let id = setInterval(() => {
                             try {
                                 let tableRows = document.querySelectorAll("div#root > div > div > div:nth-child(1) > div > table:nth-child(2) tr");
@@ -166,7 +189,7 @@ function mobileCheck() {
                     // 事件監聽：打架的四個按鈕
                     let buttons = document.querySelectorAll("div#root > div > div > div:nth-child(2) > div:nth-last-child(1) button");
                     buttons.forEach(button => {
-                        button.addEventListener("click", async () => {
+                        button.addEventListener("click", async() => {
                             if (!button.disabled) {
                                 let start = Date.now();
 
@@ -231,6 +254,44 @@ function mobileCheck() {
                     }
 
                     expTd.innerText += ` / ${nextLevelReq}`;
+                }
+            } else if (url.match(/report\/[0-9a-f]+$/)) {
+                // 在戰報頁面
+                if (url != lastUrl) {
+                    lastUrl = url;
+
+                    let reportId = url.match(/report\/([0-9a-f]+)$/)[1];
+                    let reportJson = await fetch(`https://storage.googleapis.com/kirito-1585904519813.appspot.com/reports2/${reportId}.json`).then(r => r.json());
+
+                    let attackerTableRows = document.querySelectorAll("div#root > div > div > div > div > table:nth-child(1) tr");
+                    let defenderTableRows = document.querySelectorAll("div#root > div > div > div > div > table:nth-child(2) tr");
+                    // PlayerName: 4th, Abilities: 7th ~ 15th.
+
+                    attackerTableRows[3].querySelector("td").innerHTML = `<a href="https://mykirito.com/profile/${reportJson.a.uid}">${reportJson.a.nickname}</a>`;
+                    defenderTableRows[3].querySelector("td").innerHTML = `<a href="https://mykirito.com/profile/${reportJson.b.uid}">${reportJson.b.nickname}</a>`;
+
+                    let attackerData, defenderData;
+                    if (mydata._id == reportJson.a.uid) {
+                        // 當自己為攻擊方時
+                        let playerId = reportJson.b.uid;
+                        attackerData = mydata;
+                        defenderData = await fetch(`https://mykirito.com/api/profile/${playerId}`, {
+                            "headers": {
+                                "token": token
+                            }
+                        }).then(r => r.json()).then(j => j.profile);
+                    } else {
+                        // 當自己為防禦方時
+                        let playerId = reportJson.a.uid;
+                        attackerData = await fetch(`https://mykirito.com/api/profile/${playerId}`, {
+                            "headers": {
+                                "token": token
+                            }
+                        }).then(r => r.json()).then(j => j.profile);
+                        defenderData = mydata;
+                    }
+
+                    addPointsAndLinkToReport(attackerTableRows, attackerData, defenderTableRows, defenderData);
                 }
             } else {
                 lastUrl = "";
